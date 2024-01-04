c38be2bca583b43999fccecde5ed582d18d0f42f
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-01-04 11:43:15 +0300

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""function named canUnlockAll"""
 
-def can_unlock_all(boxes):
-    open_boxes = [0]
 
-    for key in open_boxes:
+def canUnlockAll(boxes):
+    """function that determines if all the boxes can be opened"""
+    openboxes = [0]
+    for key in openboxes:
         for box in boxes[key]:
-            if box not in open_boxes and 0 <= box < len(boxes):
-                open_boxes.append(box)
-
-    return len(open_boxes) == len(boxes)
+            if box not in openboxes and box < len(boxes):
+                openboxes.append(box)
+    if len(openboxes) == len(boxes):
+        return True
+    return False
