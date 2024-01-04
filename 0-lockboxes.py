de92bc71d75bda8abc72f2d71c79e026892f13e2
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-01-04 11:16:30 +0300

    locboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """function that determines if all the boxes can be opened"""
+    openboxes = [0]
+    for key in openboxes:
+        for box in boxes[key]:
+            if box not in openboxes and box < len(boxes):
+                openboxes.append(box)
+    if len(openboxes) == len(boxes):
+        return True
+    return False
