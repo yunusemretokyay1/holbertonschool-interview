bdcfd7945d0e64f0091c33b4c278d9323bc2f01f
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-01-18 20:10:21 +0300

    Update 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,34 +1,21 @@
 #include "binary_trees.h"
-#include <stdlib.h>
-
 /**
- * binary_tree_node - TRIES TO:
- * create new binary_tree_t type node in memory,
- * with: 'value' as its 'n' int attribute, 'parent' as its 'parent'
- * (even if 'parent' is NULL),
- * and NULL children; and return its address.
- *
- * If allocating it in memory fails, this function just returns
- * NULL.
- *
- * @parent: parent of the new node
- * @value: What the 'n' int attribute of the new node will be
- *
- * Return: address of created node if the malloc was successful,
- * NULL otherwise.
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent of tree
+ * @value: value of new node
+ * Return: new node or NULL if failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *result = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	/* malloc failed */
-	if (result == NULL)
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
 		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 
-	result->n = value;
-	result->parent = parent;
-	result->left = NULL;
-	result->right = NULL;
-
-	return (result);
+	return (node);
 }
