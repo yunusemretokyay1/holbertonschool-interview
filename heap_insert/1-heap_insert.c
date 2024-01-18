c68982cfa34ca2a477ab7b0267782cd13b334cc3
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-01-18 17:53:54 +0300

    Heap - Insert

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,27 +1,60 @@
 #include "binary_trees.h"
+
+/**
+ * heap_end - Returns the address of the pointer
+ * to where the next unoccupied slot of the heap
+ * is
+ */
 heap_t **heap_end(heap_t **root, int value)
 {
-    if(root == NULL)
-    return (NULL);
-    
-    heap_t **left = heap_end((*root)->left);
-    heap_t **right = heap_end((*root)->right);
-   
-   if (*left == NULL)
+	if (root == NULL)
+		return (NULL);
+
+	heap_t **left = heap_end((*root)->left);
+	heap_t **right = heap_end((*root)->right);
+
+	if (*left == NULL)
 		return (*left);
 	if (*right == NULL)
 		return (*right);
-    
-    
-  }
+}
 
-  heap_t **heap_insert_place(heap_t **root, int value) {
+/**
+ * heap_insert_place - Returns the address of the pointer
+ * to where the new node should be inserted
+ */
+heap_t **heap_insert_place(heap_t **root, int value) {
 	if (root == NULL)
 		return (NULL);
 
 	heap_t **left = heap_end((*root));
 }
 
+/**
+ * heap_insert - TRIES TO:
+ * Create a new 'heap_t' type node with:
+ * 'value' as its 'n', into its correct place in
+ * the heap pointed to by 'root'.
+ *
+ * USING MAX-HEAP ORDERING.
+ *
+ * By 'heap' I mean all of the nodes of the heap
+ * AND THE POINTER TO THE ROOT NODE, WHICH MAY EVEN BE NULL.
+ *
+ * THIS MEANS THAT IF '*root' is NULL, THE NEW NODE WILL
+ * BECOME THE ROOT NODE OF THE HEAP!!!
+ *
+ * If 'root' is NULL or if the malloc FAILS,
+ * This function just returns NULL.
+ *
+ * @root: pointer to (L-VALUE!!) pointer to root
+ * @value: int to insert into heap into correct place
+ *
+ * Return: pointer to new heap node
+ * (in its correct place in the heap)
+ * if malloc was successful,
+ * NULL otherwise
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	if (root == NULL)
