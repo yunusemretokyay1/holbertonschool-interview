27c5dc84fedba3ada008b466d47a03bd331d93b5
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-01 16:11:34 +0100

    Sandpiles sum

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""
-calculates the fewest number of operations needed to result
-in exactly n H characters in the file.
-
+"""minoperations module
 """
 
 
 def minOperations(n):
-    operation_count = 0
-    number = 2
-    if type(n) is not int or n <= 1:
-        return operation_count
-    while n > 1:
-        if n % number == 0:
-            operation_count += number
-            n /= number
-        else:
-            number += 1
-    return(operation_count)
+    """minOperations is a method that calculates
+    the fewest number of operations needed to result
+    in exactly n H characters
+
+    Args:
+        n (int): amount of H
+
+    Return:
+        minimum number of operations (an integer)
+    """
+    minOp, div = 0, 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOp += div
+        n = int(n / div)
+    return minOp
