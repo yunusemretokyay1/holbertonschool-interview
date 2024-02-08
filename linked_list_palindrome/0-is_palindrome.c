2a5f86a2a73b4c3b85bbb4f1b519b856a2000d87
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-02-09 00:34:19 +0100

    Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,28 +1,28 @@
 #include "lists.h"
-
-int recursive_palindorme(listint_t **left, listint_t *right)
-{
-    int ispalindrome;
-
-	if (right == NULL)
-		return (1);
-
-
-	ispalindrome = recursive_palindrome(left, right->next) && 
-	(right->n == (*left)->n);
-
-	if (ispalindrome == 0)
-		return (0);
-
-
-
-
-	*left = (*left)->next;
-
-	return (ispalindrome);
-}
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of listint_t to check if is a palindrome.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
 int is_palindrome(listint_t **head)
 {
-	return (recursive_palindrome(head, *head));
+	listint_t *cpy = *head;
+	int buff[2048], i = 0, j = 0, mid;
 
+	if (!head || (cpy && !cpy->next))
+		return (1);
+	while (cpy)
+	{
+		buff[i] = cpy->n;
+		cpy = cpy->next;
+		i++;
+	}
+	i--, mid = i / 2;
+	while (i >= mid && j <= mid)
+	{
+		if (buff[j] != buff[i])
+			return (0);
+		i--, j++;
+	}
+	return (1);
 }
