fa35ec6461aa27ec110e6db227a81b823956e034
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-18 13:23:12 +0200

    Betty coding style

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -1,12 +1,8 @@
-#ifndef _SEARCH_H_
-#define _SEARCH_H_
+#ifndef SEARCH_H
+#define SEARCH_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-
-
+#include <stdio.h>
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
@@ -20,15 +16,12 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
-skiplist_t *create_skiplist(int *array, size_t size);
-void print_skiplist(const skiplist_t *list);
-void free_skiplist(skiplist_t *list);
-skiplist_t *linear_skip(skiplist_t *head, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* _SEARCH_H_ */
