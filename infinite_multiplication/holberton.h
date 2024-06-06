a8ae0857d460d5fd3a2198729a935260200c4f4a
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-06-06 03:34:40 +0200

    Infinite Multiplication

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/* LIBRARIES */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* PROTOTYPES */
+int _putchar(char c);
+#endif /* _HOLBERTON_H_ */
